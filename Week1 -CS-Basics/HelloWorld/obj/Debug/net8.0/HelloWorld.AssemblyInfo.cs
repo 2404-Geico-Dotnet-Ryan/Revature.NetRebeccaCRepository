3d38739237a2d149542dbe7c0807ce4638aa9066
RebeccaChesterNETClass <rebecca.brinson1013@gmail.com>
2024-04-18 09:17:06 -0400

    pushing changes that show this morning

--- Week1 -CS-Basics/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Week1 -CS-Basics/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c70345013fab65e802093731a9b61cee77d7da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb619839e1f5249654aabed97068001e4f5cca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
