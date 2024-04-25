b2e04ca7d3581ade0f28fce84805d5ee0743063a
RebeccaChesterNETClass <rebecca.brinson1013@gmail.com>
2024-04-25 12:55:12 -0400

    again

--- Week2 -CS-Fundamentals/IntroToClassesEx/obj/Debug/net8.0/IntroToClassesEx.AssemblyInfo.cs
+++ Week2 -CS-Fundamentals/IntroToClassesEx/obj/Debug/net8.0/IntroToClassesEx.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntroToClassesEx")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c5f5cf2f2833bcbeee61e20296d43cb7ad38982")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b449122c6ce10b861bc4e79a695e2d588f8a20e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntroToClassesEx")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntroToClassesEx")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
