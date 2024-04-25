fa58de52cdc6f2787502523e754bc06ac56ecb38
RebeccaChesterNETClass <rebecca.brinson1013@gmail.com>
2024-04-25 14:13:12 -0400

    creating my own dog method

---
+++ Week2 -CS-Fundamentals/PracticingMethods/Dog.cs
@@ -0,0 +1,24 @@
+class Dog
+{
+  //Fields
+  public string? breed;
+  public string? color;
+  public string? lengthOfHair; 
+  public int age;
+  public int numberOfLegs;
+}
+
+//no arguement constructor (can set default values)
+public Dog() 
+{
+
+}
+//Full-Arguement Constructor
+public Dog(string breed, string color, string lengthOfHair, int age, int numberOfLegs)//these parameters only override the orginial in this method not outside the method
+    {
+        this.breed = breed;
+        this.color = color;
+        this.lengthOfHair = lengthOfHair;
+        this.age = age;
+        this.numberOfLegs = numberOfLegs;
+    }
