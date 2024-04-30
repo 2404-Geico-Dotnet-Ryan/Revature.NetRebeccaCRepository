fd8da2be7ced3624055193ea3974d3700124be4a
RebeccaChesterNETClass <rebecca.brinson1013@gmail.com>
2024-04-30 13:34:52 -0400

    creating Project1

--- Week2 -CS-Fundamentals/PracticingMethods/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿ // Things to do - google some practice questions on Chat GPT etc something like below
-    //Takes in two strings and prints both of them twice
-    //Find the largest number between 3 ints
-}
