efe21dfcb55b8ed28cec33707eb235b6f618bef5
RebeccaChesterNETClass <rebecca.brinson1013@gmail.com>
2024-05-02 18:00:44 -0400

    1

--- Week1 -CS-Basics/FirstGuessingGamePractice/obj/Debug/net8.0/FirstGuessingGamePractice.AssemblyInfo.cs
+++ Week1 -CS-Basics/FirstGuessingGamePractice/obj/Debug/net8.0/FirstGuessingGamePractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstGuessingGamePractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b739c73deb1fcb30989cf5bd76c127e473c5cfed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dd25a62574f30edba803a43a109d88766e3608d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstGuessingGamePractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstGuessingGamePractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
