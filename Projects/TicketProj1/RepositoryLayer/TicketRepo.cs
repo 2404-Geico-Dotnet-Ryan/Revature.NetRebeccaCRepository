ceec685fa86f941c13d986de46ea8766267f2a23
RebeccaChesterNETClass <rebecca.brinson1013@gmail.com>
2024-05-20 10:34:23 -0400

    2

---
+++ Projects/TicketProj1/RepositoryLayer/TicketRepo.cs
@@ -0,0 +1,62 @@
+class TicketRepo
+{
+    //make at least CRUD methods here
+
+    TicketStorage ticketStorage =new();
+
+    public Ticket? AddTicket(Ticket t)
+    {
+        t.TicketId= ticketStorage.ticketIdCounter++;
+        ticketStorage.tickets.Add(t.TicketId, t);
+        return t; 
+    }
+
+    public Ticket? GetTicket(int ticketId)
+    {
+        if (ticketStorage.tickets.ContainsKey(ticketId))
+        {
+            Ticket selectedTicket = ticketStorage.tickets[ticketId];
+            return selectedTicket;
+            System.Console.WriteLine(selectedTicket);  
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid Ticket ID - Please Try Again");
+            return null;
+        }
+    }
+
+    public List<Ticket> GetAllTickets()
+    {
+        return ticketStorage.tickets.Values.ToList();
+    }
+
+    public Ticket? UpdateTicket(Ticket updatedTicket)
+    {
+        try
+        {
+            ticketStorage.tickets[updatedTicket.TicketId] = updatedTicket;
+            return updatedTicket;
+        }
+        catch (Exception)
+        {
+            System.Console.WriteLine("Invalid Ticket ID - Please Try Again");
+            return null;
+        }
+    }
+
+      public Ticket? DeleteTicket(Ticket t)
+    {
+        bool didRemove = ticketStorage.tickets.Remove(t.TicketId);
+        if (didRemove)
+        {
+            return t;
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid Ticket ID - Please Try Again");
+            return null;
+        }
+    }
+
+}
