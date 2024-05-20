c387bce1ace1e33d6b13f15751b6459adc644705
RebeccaChesterNETClass <rebecca.brinson1013@gmail.com>
2024-05-20 08:51:01 -0400

    1

---
+++ Projects/TicketProj1/Models_Objects/Ticket.cs
@@ -0,0 +1,38 @@
+    class Ticket
+    {
+    public int TicketId { get; set; }  //Ticket number common between all
+    public string Type {get; set; } // what type  of ticket (title)
+    public double Cost {get; set; } //how much ticket was for 
+    public double Balance {get;set; } //how much is left to pay on ticket
+    public bool PaidInFull { get; set; }  // true if ticket still has balance
+    public long DueDate { get; set; } //when is the ticket due
+    public User? Driver {get;set;}
+
+    public Ticket()
+    {
+        Type = "";
+    }
+
+    public Ticket (int ticketId, string type, double cost, double balance, bool paidinfull, long duedate, User? driver)
+    {
+        TicketId = ticketId;
+        Type = type;
+        Cost = cost;
+        Balance = balance;
+        PaidInFull = paidinfull;
+        DueDate = duedate;
+        Driver = driver;
+    }
+
+    public override string ToString() 
+    {
+        return "{Ticket ID:" + TicketId + 
+        ", Type:" + Type + 
+        ", Cost: " + Cost + 
+        ", Balance: " + Balance + 
+        ", PaidInFull: " + PaidInFull +
+        ", DueDate: " + DueDate + "}";
+        ", Driver: " + Driver?.ToString() + "}";
+    }
+
+}
