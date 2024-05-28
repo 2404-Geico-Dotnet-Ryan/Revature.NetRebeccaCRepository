3cc3fe7ecb0c32ed5a9e6d25ffc0927dd5431ff1
RebeccaChesterNETClass <rebecca.brinson1013@gmail.com>
2024-05-28 11:41:57 -0400

    updating study guides will add my combined notes after while

--- Week6-Frameworks/MovieEFApp/Context/MovieEFAppDBContext.cs
+++ Week6-Frameworks/MovieEFApp/Context/MovieEFAppDBContext.cs
@@ -2,12 +2,20 @@ using Microsoft.EntityFrameworkCore;
 
 class MovieEFAppDBContext : DbContext
 {
+   
     public DbSet <User> Users { get; set; } 
-    //public DbSet <Movie> Movies { get; set; } // we are not using this now so does not need to be active 
+    public DbSet <Movie> Movies { get; set; }  
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         //Read in our connection String from our txt file 
         string connectionString = File.ReadAllText(@"C:\Users\U713PY\RevatureTraining.NET\Revature.NetRebeccaCRepository\MovieApp-db.txt");
         optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+       modelBuilder.Entity<Movie>().HasOne(m=>m.Renter);
+       // this can be concatinated to add .WithMany().HasForeignKey();
+
     }
 }
