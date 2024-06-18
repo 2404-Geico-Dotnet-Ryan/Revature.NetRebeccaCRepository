c1f12cc37e218afa8c525d21ceed559e5652fb68
RebeccaChesterNETClass <rebecca.brinson1013@gmail.com>
2024-06-18 17:48:59 -0400

    1

--- Projects/Project1/obj/Debug/net8.0/Projects.AssemblyInfo.cs
+++ Projects/Project1/obj/Debug/net8.0/Projects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fbfb4667d472d6df1430716296e217a2a34a5ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+381ee8f477769f711403603dcff03be2a2e3c22b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
